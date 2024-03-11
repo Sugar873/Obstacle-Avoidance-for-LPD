fd6a5260a1ed404c609285188104ac77f31cbfdb
Sagar Syal <sagarsyal@cmail.carleton.ca>
2024-03-11 11:12:09 -0400

    added callback functionality to see when a certain operation is done

--- main.py
+++ main.py
@@ -4,6 +4,19 @@ class Meca500:
     def __init__(self, address):
         self.robot = mdr.Robot()
         self.address = address
+        self.callbacks = mdr.RobotCallbacks()
+        self.register_callbacks()
+
+    def register_callbacks(self):
+        self.callbacks.on_connected = lambda: self.on_event("Connected")
+        self.callbacks.on_disconnected = lambda: self.on_event("Disconnected")
+        self.callbacks.on_activated = lambda: self.on_event("Activated")
+        self.callbacks.on_deactivated = lambda: self.on_event("Deactivated")
+        self.callbacks.on_homed = lambda: self.on_event("Homed")
+        self.robot.RegisterCallbacks(callbacks=self.callbacks, run_callbacks_in_separate_thread=True)
+
+    def on_event(self, event):
+        print(event)
 
     def connect(self):
         self.robot.Connect(address=self.address, disconnect_on_exception=False)
@@ -21,7 +34,7 @@ class Meca500:
     def deactivate_and_disconnect(self):
         self.robot.DeactivateRobot()
         self.robot.Disconnect()
-    
+
 if __name__ == "__main__":
     robot = Meca500('192.168.0.100')  # replace with real host
     robot.connect()
